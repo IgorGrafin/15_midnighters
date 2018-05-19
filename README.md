ca28a503a8a641288e2ae87b172a711ff735466d
Igor Grafin <apaticmail@gmail.com>
2018-05-19 18:41:58 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,6 +1,36 @@
 # Night Owls Detector
+## Installation
+Install requirements.
+```pip install -r requirements.txt```
 
-[TODO. There will be project description]
+It is better to use virtual enviroments
+
+## How to use
+Just run the script
+```python3 seek_dev_nighters.py ```
+
+
+## Examples
+
+```
+python.exe E:/GitHub/devman/15_midnighters/seek_dev_nighters.py
+Midnighters:
+kesha.linge
+eNemi
+id215639888
+id185085967
+dreamfall3r
+nick__korolev
+Ruslan19308676
+fedya_kiselev
+ФедорКиселев
+ad2500
+id45197784
+id311542249
+id106957374
+AndreyArakelyants
+```
+
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,55 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import pytz
+import requests
+import datetime
 
-def get_midnighters():
-    pass
+
+def get_number_of_pages():
+    url = "https://devman.org/api/challenges/solution_attempts/"
+    response = requests.get(url)
+    if response.ok:
+        return response.json()["number_of_pages"]
+
+
+def load_attempts(pages):
+    url = "https://devman.org/api/challenges/solution_attempts/"
+    for page in range(1, pages+1):
+        response = requests.get(url, params={"page": page})
+        attempts_data = response.json()["records"]
+        for attempt in attempts_data:
+            yield {
+                'username': attempt["username"],
+                'timestamp': attempt["timestamp"],
+                'timezone': attempt["timezone"],
+            }
+
+
+def get_midnighters(attempts_data):
+    midnighters = set()
+    midnight_time = 0
+    morning_time = 5
+    for attempt in attempts_data:
+        attempt_time = timestamp_to_hours(
+            attempt["timestamp"],
+            attempt["timezone"]
+        )
+
+        if midnight_time <= attempt_time <= morning_time:
+            midnighters.add(attempt["username"])
+    return midnighters
+
+
+def timestamp_to_hours(timestamp, user_timezone):
+    return datetime.datetime.fromtimestamp(
+        timestamp,
+        tz=pytz.timezone(user_timezone)
+    ).hour
+
 
 if __name__ == '__main__':
-  pass
+    number_of_pages = get_number_of_pages()
+    attempts_data = []
+    attempts_data += load_attempts(number_of_pages)
+    midnighters = get_midnighters(attempts_data)
+    print("Midnighters:")
+    for username in midnighters:
+        print(username)
